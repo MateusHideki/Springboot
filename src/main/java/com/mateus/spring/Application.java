443fdbaaabc26c29170d2f594e85f5dcba97e002
MateusHideki <hidekisimote@hotmail.com.br>
2018-04-14 00:22:59 -0300

    adicao da classe produto e associacao com categoria

--- src/main/java/com/mateus/spring/Application.java
+++ src/main/java/com/mateus/spring/Application.java
@@ -1,22 +1,30 @@
 package com.mateus.spring;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
+import com.mateus.spring.domain.Categoria;
+import com.mateus.spring.domain.Produto;
+import com.mateus.spring.repositories.CategoriaRepository;
+import com.mateus.spring.repositories.ProdutoRepository;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan("com.mateus.spring")
-public class Application extends SpringBootServletInitializer {
+public class Application implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	private static final Class<Application> applicationClass = Application.class;
 
 	public static void main(String[] args) {
@@ -24,9 +32,26 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(applicationClass, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
+	public void run(String... args) throws Exception{
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
+	
+	
+	
 }
--- src/main/java/com/mateus/spring/domain/Categoria.java
+++ src/main/java/com/mateus/spring/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.mateus.spring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -13,20 +16,23 @@ public class Categoria implements Serializable {
 	 * serialVersionUID para Categoria
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
-	public Categoria() {
-
-	}
-
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
+	
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
+	}
+
+	public Categoria() {
+
 	}
 
 	public Integer getId() {
@@ -43,6 +49,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -74,5 +88,4 @@ public class Categoria implements Serializable {
 	public String toString() {
 		return "Categoria [id=" + id + ", nome=" + nome + "]";
 	}
-
 }
